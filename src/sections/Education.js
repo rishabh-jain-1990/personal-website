8ae0d9755400cf32248613fb7682fa9a5efdc9f8
Rishabh Jain <rishabh.1990.j@gmail.com>
2024-03-15 13:30:35 +0530

    work experience content

--- src/sections/Education.js
+++ src/sections/Education.js
@@ -6,15 +6,19 @@ export default function Education() {
     return (
         <div className="Section-Default Education">
           <div className="Education-Item" style={{flex: 2}}> 
-            <h1 style={{ color: 'var(--primary-dark)',  }}>Newcastle University, UK</h1>
-            <h2 style={{ color: 'var(--primary-dark)', marginTop:'10px', marginBottom:'10%'}}>B. Eng., Electronics & Computer Engineering, 2009 - 2011</h2>
-
-            <h1 style={{ color: 'var(--primary-dark)', marginTop:'10%' }}>Jaypee Institue of Information Technology, JIIT</h1>
-            <h2 style={{ color: 'var(--primary-dark)', marginTop:'10px'}}>B. Tech., Computer Science Engineering, 2008 - 2009</h2>
+          <div>
+            <h1 style={{ color: 'var(--primary-dark)'}}>Newcastle University, UK</h1>
+            <h2 style={{ color: 'var(--primary-dark)', marginTop: '10px'}}>B. Eng., Electronics & Computer Engineering, 2009 - 2011</h2>
+            </div>
+            <div>
+            <h1 style={{ color: 'var(--primary-dark)'}}>Jaypee Institue of Information Technology, JIIT</h1>
+            <h2 style={{ color: 'var(--primary-dark)', marginTop: '10px'}}>B. Tech., Computer Science Engineering, 2008 - 2009</h2>
+            </div>
         </div>
+
         <div className="Education-Item" style={{flex: 1, alignItems:'flex-end', marginRight:'5%'}}> 
-          <img src={ncl_logo} style={{width:'80%', marginBottom:'15%'}}/>
-          <img src={jiit_logo} style={{width:'30%', marginTop:'10%'}}/>
+          <img src={ncl_logo} style={{width:'80%'}}/>
+          <img src={jiit_logo} style={{width:'30%'}}/>
         </div>
          </div>
       );
--- src/sections/Work Experience.js
+++ src/sections/Work Experience.js
@@ -1,5 +1,24 @@
 import React from "react";
+import default_logo from '../assets/Logo_Blue_White_BG.png';
+import bs_logo from '../assets/Bowstring Studio - Logo - Box White copy.png';
 
 export default function WorkExperience() {
-    return (null);
+    return (
+        <div className="Section-Default Work-Experience" >
+          <div className="Education-Item" style={{flex: 2}}> 
+          <div>
+            <h1 style={{ color: 'var(--primary-dark)'}}>Default</h1>
+            <h2 style={{ color: 'var(--primary-dark)', marginTop: '10px'}}>Co Founder & CTO, 2019 - Current</h2>
+            </div>
+            <div>
+            <h1 style={{ color: 'var(--primary-dark)'}}>Bowstring Studio</h1>
+            <h2 style={{ color: 'var(--primary-dark)', marginTop: '10px'}}>Co Founder & Mobile Lead, 2013 - 2019</h2>
+            </div>
+        </div>
+        <div className="Education-Item" style={{flex: 1, alignItems:'flex-end', marginRight:'5%'}}> 
+          <img src={default_logo} style={{width:'80%'}}/>
+          <img src={bs_logo} style={{width:'80%'}}/>
+        </div>
+         </div>
+      );
 }
