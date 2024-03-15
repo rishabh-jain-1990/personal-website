f435d78d3c6b91001fbbb5eb2cc7e75433e6d470
Rishabh Jain <rishabh.1990.j@gmail.com>
2024-03-15 14:34:27 +0530

    social media content

--- src/sections/Social Media.js
+++ src/sections/Social Media.js
@@ -1,5 +1,26 @@
 import React from "react";
+import { ReactComponent as InstaLogo } from '../assets/Instagram_Glyph_Black.svg';
+import { ReactComponent as GitLogo } from '../assets/github-mark.svg';
+import { ReactComponent as LiLogo } from '../assets/icons8-linkedin.svg';
+import { ReactComponent as XLogo } from '../assets/x_logo.svg';
+import { openInNewTab } from '../utils/Methods.js';
 
 export default function SocialMedia() {
-    return (null);
+
+    return (
+        <div className='Section-Default Social-Media'>
+        <div onClick={() => openInNewTab('https://www.instagram.com/rjain90')} style={{cursor:"pointer"}}>
+            <InstaLogo fill='white' style={{width:'100px', height: '100px'}}/>
+        </div>
+        <div onClick={() => openInNewTab('https://github.com/rishabh-jain-1990')} style={{cursor:"pointer"}}>
+            <GitLogo fill='white' style={{width:'100px', height: '100px'}}/>
+        </div>
+        <div onClick={() => openInNewTab('https://www.linkedin.com/in/rjain90')} style={{cursor:"pointer"}}>
+            <LiLogo fill='white' style={{width:'100px', height: '100px'}}/>
+        </div>
+        <div onClick={() => openInNewTab('https://twitter.com/rjain90')} style={{cursor:"pointer"}}>
+            <XLogo fill='white' style={{width:'100px', height: '100px'}}/>
+        </div>
+        </div>
+    );
 }
