11d8f1548bf1f139b708fe75dfee1efb11fa4e9a
Rishabh Jain <rishabh.1990.j@gmail.com>
2024-03-15 17:30:57 +0530

    heading work ex

--- src/sections/Work Experience.js
+++ src/sections/Work Experience.js
@@ -4,21 +4,23 @@ import bs_logo from '../assets/Bowstring Studio - Logo - Box White copy.png';
 
 export default function WorkExperience() {
     return (
-        <div className="Section-Default Work-Experience" >
-          <div className="Education-Item" style={{flex: 2}}> 
-          <div>
-            <h1 style={{ color: 'var(--primary-dark)'}}>Default</h1>
-            <h2 style={{ color: 'var(--primary-dark)', marginTop: '10px'}}>Co Founder & CTO, 2019 - Current</h2>
+        <div className="Section-Default Work-Experience">
+          <h1 style={{ color: 'var(--primary-dark)', marginTop:'5%'}}>Work Experience</h1>
+
+          <div className="Education-Item" style={{flexDirection:"row", justifyContent:'space-between', width:'100%', marginTop:'10%'}}> 
+            <div style={{flexDirection:"column"}}>
+              <h1 style={{ color: 'var(--primary-dark)'}}>Default</h1>
+              <h2 style={{ color: 'var(--primary-dark)', marginTop: '10px'}}>Co Founder & CTO, 2019 - Current</h2>
             </div>
-            <div>
-            <h1 style={{ color: 'var(--primary-dark)'}}>Bowstring Studio</h1>
-            <h2 style={{ color: 'var(--primary-dark)', marginTop: '10px'}}>Co Founder & Mobile Lead, 2013 - 2019</h2>
+            <img src={default_logo} style={{ width:'200px'}}/>
+          </div>
+          <div className="Education-Item" style={{flexDirection:"row", width:'100%', justifyContent:'space-between', marginTop:'10%'}}> 
+            <div style={{flexDirection:"column"}}>
+              <h1 style={{ color: 'var(--primary-dark)'}}>Bowstring Studio</h1>
+              <h2 style={{ color: 'var(--primary-dark)', marginTop: '10px'}}>Co Founder & Mobile Lead, 2013 - 2019</h2>
             </div>
-        </div>
-        <div className="Education-Item" style={{flex: 1, alignItems:'flex-end', marginRight:'5%'}}> 
-          <img src={default_logo} style={{width:'80%'}}/>
-          <img src={bs_logo} style={{width:'80%'}}/>
-        </div>
+            <img src={bs_logo} style={{ width:'200px'}}/>
+          </div>
          </div>
       );
 }
