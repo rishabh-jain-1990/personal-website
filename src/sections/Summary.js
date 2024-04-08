7c59287465ad10e97adff99ea762fce319af6dd7
Rishabh Jain <rishabh.1990.j@gmail.com>
2024-04-08 20:51:46 +0530

    summary text weight

--- src/sections/Summary.js
+++ src/sections/Summary.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "../App.css";
-import {summary_text} from "../utils/Constants";
-
+import { summary_text } from "../utils/Constants";
 
 export default function Summary() {
   return (
     <div className="Section-Default Summary">
-      <h2 style={{ color: 'var(--primary-dark)', textAlign:'center', marginLeft: '5%', marginRight:'5%', 
-    lineHeight: 2}}>{summary_text}</h2>
+      <h2
+        style={{
+          color: "var(--primary-dark)",
+          textAlign: "center",
+          marginLeft: "5%",
+          marginRight: "5%",
+          lineHeight: 2,
+          fontWeight: "300",
+        }}
+      >
+        {summary_text}
+      </h2>
     </div>
   );
 }
