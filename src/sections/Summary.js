d21195ef59a105e14f52948644a329411c41e434
Rishabh Jain <rishabh.1990.j@gmail.com>
2024-04-30 19:04:22 +0530

    summary style

--- src/sections/Summary.js
+++ src/sections/Summary.js
@@ -5,32 +5,23 @@ import { summary_text } from "../utils/Constants";
 export default function Summary() {
   return (
     <div className="Section-Default Summary">
-      {/* <h2
-        style={{
-          color: "var(--primary-dark)",
-          textAlign: "center",
-          marginLeft: "5%",
-          marginRight: "5%",
-          lineHeight: 2,
-          fontWeight: "300",
-          whiteSpace: "pre-wrap",
-        }}
-      >
-        {summary_text}
-      </h2> */}
       <div
         dangerouslySetInnerHTML={{ __html: summary_text }}
-        style={{
-          color: "var(--primary-dark)",
-          textAlign: "center",
-          marginLeft: "5%",
-          marginRight: "5%",
-          lineHeight: 2,
-          fontWeight: "300",
-          fontSize: "21px",
-          whiteSpace: "pre-wrap",
-        }}
+        style={styles.summary_text}
       />
     </div>
   );
+}
+
+const styles = {
+    summary_text:{
+        color: "var(--primary-dark)",
+        textAlign: "center",
+        marginLeft: "5%",
+        marginRight: "5%",
+        lineHeight: 2,
+        fontWeight: "300",
+        fontSize: "21px",
+        whiteSpace: "pre-wrap",
+      }
 }
