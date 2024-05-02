5157cc425365e3165b4aaf0823883f0a8b4844fa
Rishabh Jain <rishabh.1990.j@gmail.com>
2024-05-02 14:08:11 +0530

    hero white

--- src/sections/hero/index.jsx
+++ src/sections/hero/index.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import hero_img from "../../assets/1610519214747_plus.jpg";
 import { hero_title, hero_subtitle } from "../../utils/Constants";
-import "./index.css"
-import "../../App.css"
+import "./index.css";
+import "../../App.css";
 
 export default function Hero() {
   return (
     <div className="Section-Default hero">
       <div>
-        <h1 style={{ color: "var(--primary-dark)" }}>{hero_title}</h1>
-        <h3 style={{ color: "var(--primary-dark)", marginTop: "10px" }}>
+        <h1 style={{ color: "var(--secondary-darker)" }}>{hero_title}</h1>
+        <h3 style={{ color: "var(--secondary-darker)", marginTop: "10px" }}>
           {hero_subtitle}
         </h3>
       </div>
